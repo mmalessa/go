98c61061088c4abed9de85a650aa5e07282b17f9
Marcin Malessa <marcin@malessa.pl>
2022-07-31 18:34:01 +0200

    Fix Transport interface

--- transport/transport.go
+++ transport/transport.go
@@ -4,5 +4,5 @@ import "github.com/mmalessa/mmessenger/envelope"
 
 type Transport interface {
 	Publish(message *envelope.Envelope) error
-	Subscribe(messageChannel chan (*envelope.Envelope), errorChannel chan (error))
+	Subscribe(busMessageChannel chan (*envelope.Envelope), busErrorChannel chan (error))
 }
